577d67a9b3625e17524a6c9ceb7654584faeb1ba
Im_vireoff_04 <87600216+VirendrKumarYadav@users.noreply.github.com>
2023-12-28 09:40:42 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import Header from './componant/Header/Header'
 import Footer from './componant/Footer/Footer'
 import Main from './componant/main/Main'
-
+import data from './lib'
 
 function App() {
   return (
     <div className="App">
    <Header/>
-    <Main/>
+    <Main jsonData={data}/>
 
 
   <Footer/>
--- src/componant/main/Main.js
+++ src/componant/main/Main.js
@@ -1,12 +1,46 @@
-import React from 'react'
-import Lib from '../../lib'
-const main = () => {
+import React,{useState} from 'react'
+import './main.css'
+
+const Main = (props) => {
+const [listData,setListData]=useState(props.jsonData);
+
+  let filterChanageData=(e)=>{
+    console.log(e.target.value);
+    let serData=e.target.value;
+    if(serData!=""){
+      const filteredData = listData.filter(item => item.rname.includes(serData)
+      );
+      setListData(filteredData);
+    }else{
+      setListData(props.jsonData);
+      
+    }
    
+  }
   return (
-    <div>
- 
+    <div className='container'>
+     
+      <div className='filter'>
+      <label>Filter </label>
+        <input onChange={(e)=>filterChanageData(e)}></input>
+      </div>
+      {
+        listData.map((data, idx) => {
+
+          return (
+            <div className='container'>
+              <div className="subContainer">
+                <h3>{data.rname}</h3>
+                <p>Rating : {data.rating} </p>
+                <div className="btns">
+                </div>
+              </div>
+
+            </div> ) 
+        })
+      }
     </div>
   )
 }
 
-export default main
+export default Main
